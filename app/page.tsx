aeee878b534246e7ddee602f14647904173429fe
juan.moncayom <juan.moncayo627@gmail.com>
2024-10-26 15:09:32 -0500

    preuba 6

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-'use client'
-
-import { useState } from 'react'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Checkbox } from "@/components/ui/checkbox"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Label } from "@/components/ui/label"
-import { Textarea } from "@/components/ui/textarea"
-import { Button } from "@/components/ui/button"
-import { CarIcon, MapIcon, MoonIcon, HelpCircleIcon } from "lucide-react"
 import Image from 'next/image'
 import Link from 'next/link'
 
